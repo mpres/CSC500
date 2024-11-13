6b043a9f990f1bbc16a5ca958a31e27d5f82e2bc
Michael Presley <mpresley84@MacBook-Pro.local>
2024-11-12 21:49:55 -0800

    Add Module 1 part A program

---
+++ Module_1_A.py
@@ -0,0 +1,23 @@
+# Mpresley 11/12/24, Module 1 CSC500, adding and subtracting two numbers
+
+print("Please enter two numbers to 'Add' and 'Substract'")
+
+#Get first number
+print("Enter Number 1")
+number1 = int(input())
+
+#Get second number
+print("Now enter Number 2")
+number2 = int(input())
+
+#Calculate the answer for addition
+answer = number1 + number2
+
+#Print out the answer for addition
+print(f'Adding {number1} and {number2} equals {answer}')
+
+#Calcuate the answer for substraction
+answer = number1 - number2
+
+print(f'Subtracting {number2} from {number1} equals {answer}')
+ 
